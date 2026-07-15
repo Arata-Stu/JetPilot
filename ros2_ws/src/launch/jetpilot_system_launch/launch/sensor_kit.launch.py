23af70cfbe36e8340354c1e6b18f1f6b62c55ea9
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2026-07-15 15:39:28 +0900

    refactor: restructure sensor launch description to improve clarity and organization

--- ros2_ws/src/launch/jetpilot_system_launch/launch/sensor_kit.launch.py
+++ ros2_ws/src/launch/jetpilot_system_launch/launch/sensor_kit.launch.py
@@ -5,22 +5,8 @@ import isaac_ros_launch_utils.all_types as lut
 from launch.conditions import IfCondition
 
 
-def generate_launch_description() -> lut.LaunchDescription:
-    args = lu.ArgumentContainer()
-
-    args.add_arg('enable_sensor_interface', True, cli=True)
-    args.add_arg('sensor_interface_pkg', 'jetpilot_system_launch', cli=True)
-    args.add_arg('sensor_interface_launch', 'launch/sensors/realsense.launch.py', cli=True)
-    args.add_arg('enable_realsense', True, cli=True)
-    args.add_arg('camera_name', 'realsense', cli=True)
-    args.add_arg('container_name', 'sensor_kit_container', cli=True)
-    args.add_arg('run_standalone', True, cli=True)
-    args.add_arg('enable_depth', False, cli=True)
-    args.add_arg('enable_color', False, cli=True)
-    args.add_arg('use_sim_time', False, cli=True)
-
-    actions = args.get_launch_actions()
-    actions.append(
+def add_sensor_interface(args: lu.ArgumentContainer):
+    return [
         lu.include(
             str(args.sensor_interface_pkg),
             str(args.sensor_interface_launch),
@@ -36,8 +22,7 @@ def generate_launch_description() -> lut.LaunchDescription:
                 lu.is_true(args.enable_sensor_interface),
                 lu.is_true(args.enable_realsense),
             )),
-        ))
-    actions.append(
+        ),
         lu.log_info([
             'Sensor interface: ',
             args.sensor_interface_pkg,
@@ -50,6 +35,24 @@ def generate_launch_description() -> lut.LaunchDescription:
                 lu.is_true(args.enable_sensor_interface),
                 lu.is_true(args.enable_realsense),
             )),
-        ))
+        ),
+    ]
 
-    return lut.LaunchDescription(actions)
+
+def generate_launch_description() -> lut.LaunchDescription:
+    args = lu.ArgumentContainer()
+
+    args.add_arg('enable_sensor_interface', True, cli=True)
+    args.add_arg('sensor_interface_pkg', 'jetpilot_system_launch', cli=True)
+    args.add_arg('sensor_interface_launch', 'launch/sensors/realsense.launch.py', cli=True)
+    args.add_arg('enable_realsense', True, cli=True)
+    args.add_arg('camera_name', 'realsense', cli=True)
+    args.add_arg('container_name', 'sensor_kit_container', cli=True)
+    args.add_arg('run_standalone', True, cli=True)
+    args.add_arg('enable_depth', False, cli=True)
+    args.add_arg('enable_color', False, cli=True)
+    args.add_arg('use_sim_time', False, cli=True)
+
+    args.add_opaque_function(add_sensor_interface)
+
+    return lut.LaunchDescription(args.get_launch_actions())
